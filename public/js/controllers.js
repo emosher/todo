f95c6f5548f395d4871d8f1517338721a0606731
emosher <emoshmosh@gmail.com>
2013-01-27 22:39:48 -0500

    Implemented front end for CRUD API.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -3,7 +3,24 @@
 /* Controllers */
 
 function ListCtrl($scope, TaskDB) {
+    // Get tasks list
     $scope.tasks = TaskDB.query();
+    
+    // Update when checkbox changes
+    $scope.update = function(task) {
+        TaskDB.update(task);
+    };
+    
+    // Remove finished tasks
+    $scope.remove = function() {
+        angular.forEach($scope.tasks, function(task) {
+            if (task.done) {
+                TaskDB.remove({id: task["_id"]});
+            }
+        });
+        // Reload tasks
+        $scope.tasks = TaskDB.query();
+    };
 }
 
 // ListCtrl.$inject = [ "$scope", "TaskDB" ];
--- public/js/services.js
+++ public/js/services.js
@@ -5,6 +5,6 @@
 angular.module("myApp.services", ["ngResource"])
     .factory("TaskDB", function($resource) {
         
-        return $resource("/api/tasks/");
+        return $resource("/api/tasks/:id", { id: "@_id" }, { update: { method: "PUT" } });
         
     });
