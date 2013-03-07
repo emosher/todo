fc01bcfb6aaba86f3717003363d6e12c666fc021
emosher <emoshmosh@gmail.com>
2013-03-06 21:10:34 -0500

    Removed routes and views. Now using express' static to serve views. Updated app.js to handle routing via Angular on the frontend.

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,8 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider.when('/list', {templateUrl: 'partials/list', controller: ListCtrl});
-    $routeProvider.when('/add', {templateUrl: 'partials/add', controller: AddCtrl});
+    $routeProvider.when('/list', {templateUrl: 'partials/list.html', controller: ListCtrl});
+    $routeProvider.when('/add', {templateUrl: 'partials/add.html', controller: AddCtrl});
     $routeProvider.otherwise({redirectTo: '/list'});
     $locationProvider.html5Mode(true);
   }]);
--- server.js
+++ server.js
@@ -4,24 +4,16 @@
  */
 
 var express = require("express"),
-    routes = require("./routes"),
-    api = require("./routes/api");
+    api = require("./api");
 
 var app = module.exports = express();
 
 // Shared configurations
-app.configure(function(){
-    app.set("views", __dirname + "/views");
-    // This seems odd - find another way to do views with HTML
-    app.engine(".html", require("ejs").renderFile);
-    app.set("view engine", "html");
-    app.set("view options", {
-        layout: false
-    }); 
+app.configure(function() {
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.static(__dirname + "/public"));
     app.use(app.router);
+    app.use(express.static(__dirname + "/public"));
 });
 
 // Dev configs
@@ -33,10 +25,6 @@ app.configure("development", function(){
 app.configure("production", function(){
     app.use(express.errorHandler());
 });
-
-// Routes
-app.get("/", routes.index);
-app.get("/partials/:name", routes.partials);
 
 // GET
 app.get("/api/tasks", api.tasks);
@@ -50,9 +38,6 @@ app.put("/api/tasks/:id", api.edit);
 
 // DELETE
 app.del("/api/tasks/:id", api.remove);
-
-// redirect all others to the index (HTML5 history)
-app.get("*", routes.index);
 
 // Start server
 app.listen(3000, function(){
